eb9c77f23f2a0f66b8d8783fcda60861daec1ff5
Richard Jones <iamrichardjones@gmail.com>
2016-02-07 21:17:37 +0000

    extra tests (amazing how a break from the code finds holes!)

--- src/test/java/core/MnemonicMapTest.java
+++ src/test/java/core/MnemonicMapTest.java
@@ -67,12 +67,51 @@ public class MnemonicMapTest {
     }
 
     @Test
+    public void testAddToMap_MatchingMnemonicDetailCtor_AddSameMnemonicDetailTwice() {
+        MnemonicMap mnemonicMap = new MnemonicMap();
+        assertEquals(0, mnemonicMap.getMap().size());
+
+        mnemonicMap.add(new MatchingMnemonic("bat", new MatchingMnemonicDetail("banana apple tomato", "MyOrigin", "MyCategory")));
+        mnemonicMap.add(new MatchingMnemonic("bat", new MatchingMnemonicDetail("banana apple tomato", "MyOrigin", "MyCategory")));
+
+        assertEquals(1, mnemonicMap.getMap().size());
+
+        List<String> list = new ArrayList<String>();
+        list.addAll(mnemonicMap.getMap().keySet());
+
+        assertEquals("Should be upper case and ordered alphabetically", "ABT", list.get(0));
+
+        Collection<List<MatchingMnemonic>> values = mnemonicMap.getMap().values();
+        assertEquals(1, values.size());
+
+        List<MatchingMnemonic> next = values.iterator().next();
+        assertEquals("Two values against one mnemonic key", 2, next.size());
+
+        {
+            MatchingMnemonic matchingMnemonic = next.get(0);
+            assertEquals("bat", matchingMnemonic.getMnemonic());
+            assertEquals("banana apple tomato", matchingMnemonic.getDetail().getExpandedMnemonic());
+            assertEquals("MyOrigin", matchingMnemonic.getDetail().getOrigin());
+            assertEquals("MyCategory", matchingMnemonic.getDetail().getCategory());
+        }
+        {
+            MatchingMnemonic matchingMnemonic = next.get(1);
+            assertEquals("bat", matchingMnemonic.getMnemonic());
+            assertEquals("banana apple tomato", matchingMnemonic.getDetail().getExpandedMnemonic());
+            assertEquals("MyOrigin", matchingMnemonic.getDetail().getOrigin());
+            assertEquals("MyCategory", matchingMnemonic.getDetail().getCategory());
+        }
+    }
+
+    @Test
     public void testGetFromMap() {
         MnemonicMap mnemonicMap = new MnemonicMap();
         MatchingMnemonic originalInput = new MatchingMnemonic("bac", "banana apple cherry");
         mnemonicMap.add(originalInput);
 
-        assertEquals(new ArrayList(), mnemonicMap.getValues("xyz"));
+        assertEquals("xyz not in there", new ArrayList<MatchingMnemonic>(), mnemonicMap.getValues("xyz"));
+
+        //and now the combination of abc - check they return something
         assertNotNull(mnemonicMap.getValues("abc"));
         assertNotNull(mnemonicMap.getValues("bac"));
         assertNotNull(mnemonicMap.getValues("bca"));
@@ -82,8 +121,11 @@ public class MnemonicMapTest {
 
         List<MatchingMnemonic> cab = mnemonicMap.getValues("cab");
         assertEquals(1, cab.size());
-        assertEquals(originalInput, cab.get(0));
+        assertEquals("original input should match", originalInput, cab.get(0));
+
         assertEquals(mnemonicMap.getValues("cba"), mnemonicMap.getValues("cab"));
-        assertEquals(mnemonicMap.getValues("bac"), mnemonicMap.getValues("abc"));
+        assertEquals(mnemonicMap.getValues("bac"), mnemonicMap.getValues("bca"));
+        assertEquals(mnemonicMap.getValues("abc"), mnemonicMap.getValues("acb"));
+        assertEquals(mnemonicMap.getValues("abc"), mnemonicMap.getValues("cba"));
     }
 }
--- src/test/java/helpers/TestUtils.java
+++ src/test/java/helpers/TestUtils.java
@@ -1,17 +1,60 @@
 package helpers;
 
 import junit.framework.AssertionFailedError;
+import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestUtils {
 
-    public static void assertContains(String value, Collection<String> list) {
-        for (String s : list) {
-            if (value.equals(s)) {
-                return;
+    public static void assertContains(String value, Collection<String> inputList) {
+        if (inputList != null && value != null) {
+            for (String s : inputList) {
+                if (value.equals(s)) {
+                    return;
+                }
             }
         }
         throw new AssertionFailedError(String.format("String %s not found in list", value));
+    }
+
+    @Test
+    public void testAssertContains_DoesContain(){
+        List<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+
+        assertContains("one", list);
+    }
+
+    @Test
+    public void testAssertContains_DoesNotContain(){
+        List<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+
+        //check error message here too
+        try {
+            assertContains("three", list);
+            fail("Should not get here - three is not part of list");
+        }
+        catch (AssertionFailedError error) {
+            assertEquals("String three not found in list", error.getMessage());
+        }
+    }
+
+    @Test(expected = AssertionFailedError.class)
+    public void testAssertContains_NullList(){
+        assertContains("", null);
+    }
+
+    @Test(expected = AssertionFailedError.class)
+    public void testAssertContains_NullValue(){
+        assertContains(null, new ArrayList<String>());
     }
 }
