0e185b920040cb0b2359d64b8fe1617aa0d5e1b3
Richard Jones <iamrichardjones@gmail.com>
2016-02-07 21:18:33 +0000

    remove commented out code

--- src/main/java/utils/ResourceBundler.java
+++ src/main/java/utils/ResourceBundler.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 public class ResourceBundler {
 
     private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("I18NMessages", Locale.getDefault());
-//    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("I18NMessages", new Locale("de", "DE"));
 
     public static String get(String key) {
         return BUNDLE.getString(key);
